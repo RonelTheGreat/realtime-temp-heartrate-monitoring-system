1fb37ed32a4e7374db28d8ea9aa8b9ef001345dc
RonelTheGreat <ronelcarlo.berino@gmail.com>
2020-12-20 13:47:27 +0800

    code cleanup

--- app.js
+++ app.js
@@ -10,13 +10,9 @@ const io = socket(http);
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 
-console.log("initializing ....");
-
 // helpers
 require("./helpers/global-variables");
 const socketEvents = require("./helpers/socket-events");
-
-console.log("initializing socket events and global vars ....");
 
 // mongodb connection
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
@@ -24,15 +20,11 @@ const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 db.once("open", async () => console.log("-- connected to DB"));
 
-console.log("initializing DB CONNECTION ....");
-
 // express middlewares
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
-
-console.log("initializing express middlewares ....");
 
 // routes
 app.use("/contacts", require("./routes/contacts"));
@@ -40,8 +32,6 @@ app.use("/", require("./routes/home"));
 app.use("/sign-in", require("./routes/signIn"));
 app.use("/simulator", require("./routes/simulator"));
 app.use("/handle-call", require("./routes/callHandler"));
-
-console.log("initializing all routes ....");
 
 // socket events listener
 socketEvents.listen(io);
